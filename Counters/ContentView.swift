24b6f50577ede247366d75ef13f50e0dfd94b234
MineTurtlee <97010924+MineTurtlee@users.noreply.github.com>
2025-08-09 20:59:13 +0700

    Update ContentView.swift

--- Counters/ContentView.swift
+++ Counters/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
                     }, label: {
                         Text("Reset...")
                     })
-                    .popover(isPresented: $ResetButtonClicked) {
+                    .sheet(isPresented: $ResetButtonClicked) {
                         ResetSelector()
                     }
                 }
